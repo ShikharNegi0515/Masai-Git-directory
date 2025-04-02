28b3bc8074eda2e580b9f145e3ac6245e28c5451
Shikhar Negi <shikharnegi31@gmail.com>
2025-04-02 17:08:53 +0530

    Adding a PrintMatrix.js

---
+++ PrintMatrix.js
@@ -0,0 +1,16 @@
+function PrintMatrix(matrix,n,m) {
+    for(let i=0;i<n;i++) {
+      let str = ""
+      for(let j=0;j<m;j++) {
+        str += matrix[i][j]
+      }
+      console.log(str)
+    }
+  }
+  
+  let matrix = [[1,2],[3,4],[5,6]]
+  let n = 3 
+  let m=2
+  
+  PrintMatrix(matrix,n,m)
+  
