58b016576b8f201b6e3a6cc9c9b9502a43fa21aa
Shikhar Negi <shikharnegi31@gmail.com>
2025-04-11 18:56:21 +0530

    Added a new assignment

---
+++ Closures in JavaScript/Closure.js
@@ -0,0 +1,10 @@
+function outer() {
+    let message = "Hello from the closure!";
+    return function inner() {
+        console.log(message)
+    }
+}
+
+let closure = outer()
+closure()
+closure()
