16c7c7f39bc7e9824a8e567c5f56b38cf5800379
Shikhar Negi <shikharnegi31@gmail.com>
2025-07-06 05:09:02 +0530

    Added a new Assignment

--- Unit 4/Evaluation/PAI1/my-react-app/src/App.jsx
+++ Unit 4/Evaluation/PAI1/my-react-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Tasks from './Pages/Tasks';
 import About from './Pages/About';
 import { useEffect, useState } from 'react';
--- Unit 4/Evaluation/PAI1/my-react-app/src/Pages/Tasks.jsx
+++ Unit 4/Evaluation/PAI1/my-react-app/src/Pages/Tasks.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 export default function Tasks({ tasks, loading, addTask, toggleCompletion }) {
     const [title, setTitle] = useState('');
