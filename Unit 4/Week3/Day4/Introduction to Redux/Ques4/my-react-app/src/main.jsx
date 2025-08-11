c45135696835964cf8eebe215d4173de8179fd5b
Shikhar Negi <shikharnegi31@gmail.com>
2025-08-11 22:14:27 +0530

    Updated the Assignment

--- Unit 4/Week3/Day4/Introduction to Redux/Ques4/my-react-app/src/main.jsx
+++ Unit 4/Week3/Day4/Introduction to Redux/Ques4/my-react-app/src/main.jsx
@@ -1,12 +1,9 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>
 )
